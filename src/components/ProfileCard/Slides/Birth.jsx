fe5df79562ae0e3a53b33c58893b3758d49c410f
Arun <friday99@outlook.it>
2021-07-10 14:27:54 +0200

    added google location to Birth

--- src/components/ProfileCard/Slides/Birth.jsx
+++ src/components/ProfileCard/Slides/Birth.jsx
@@ -1,13 +1,13 @@
 import {
-  Text,
-  Image,
-  Flex
+  Box,
+  Flex,
+  Text
 } from '@chakra-ui/react'
 
 export default function Birth ({ user }) {
   return (
     <Flex
-      width='80%'
+      w='80%'
       display='flex'
       flexDirection='column'
       alignItems='center'
@@ -15,21 +15,25 @@ export default function Birth ({ user }) {
       background='hsla(0, 0%, 100%, .2)'
       boxShadow='0px 3px 5px hsla(0, 0%, 0%, .2)'
       borderRadius='20px'
+      p='1.5rem 0'
     >
       <Text variant='info-title'>
         Date Of Birth
       </Text>
       <Text variant='info'>
-        {user.dateOfBirth.slice(0, 10).replaceAll('-', '/')}
+        {user.dateOfBirth ? user.dateOfBirth.slice(0, 10).replaceAll('-', '/') : 'Unavailable'}
       </Text>
       <Text variant='info-title' mt='1rem'>
         Birth Location
       </Text>
       <Text variant='info'>
-        {user.birthLocation.terms.slice(-3).map(({ value }) => value).join(', ')}
+        {user.birthLocation ? user.birthLocation.terms.slice(-3).map(({ value }) => value).join(', ') : 'Unavailable'}
       </Text>
-      <Image
-        src='https://assets-global.website-files.com/6050a76fa6a633d5d54ae714/609147088669907f652110b0_report-an-issue(about-maps).jpeg'
+      <Box
+        as='iframe'
+        src={`https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_GOOGLE_LOCATION_API_KEY}&q=place_id:${user.birthLocation.place_id}&zoom=10`}
+        loading='lazy'
+        title='birth-location'
         w='85%'
         minH='20%'
         mt='1rem'
