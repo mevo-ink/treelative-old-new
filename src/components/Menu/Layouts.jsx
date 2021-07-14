a0cf6fba2b75f19c2ee66e9f27fd4856cc5670b7
Arun <friday99@outlook.it>
2021-07-14 15:59:28 +0200

    refactored Layouts styles

--- src/components/Menu/Layouts.jsx
+++ src/components/Menu/Layouts.jsx
@@ -1,35 +1,58 @@
-import { Text, Box, Button } from '@chakra-ui/react'
+import { Text, Box, Image, Button } from '@chakra-ui/react'
 
 import { useSetRecoilState } from 'recoil'
 import { layoutAtom } from 'utils/atoms.js'
 
+import age from 'images/ageView.png'
+import map from 'images/mapView.png'
+import network from 'images/graphView.png'
+
 export default function Layouts ({ onClose }) {
   const setLayout = useSetRecoilState(layoutAtom)
 
-  const handleLayoutChange = (layout) => {
-    setLayout(layout)
+  const iconsAndNames = [
+    { icon: network, name: 'network' },
+    { icon: map, name: 'map' },
+    { icon: age, name: 'age' }
+  ]
+
+  const handleLayoutChange = async (layout) => {
     onClose()
+    await new Promise(resolve => setTimeout(resolve, 110))
+    setLayout(layout)
   }
 
   return (
     <Box>
       <Text
-        fontSize='1.3rem'
         mb='.5rem'
-        color='hsla(0, 0%, 100%, .95)'
+        color='hsla(0, 0%, 100%, .8)'
       >
         Layouts
       </Text>
       <Box
         w='100%'
         h='75px'
-        bg='hsla(0, 0%, 100%, .95)'
         borderRadius='10px'
-        boxShadow='inset 0 0px 5px #000000'
       >
-        <Button colorScheme='red' onClick={() => handleLayoutChange('network')}>GRAPH</Button>
-        <Button colorScheme='red' onClick={() => handleLayoutChange('map')}>MAP</Button>
-        <Button colorScheme='red' onClick={() => handleLayoutChange('age')}>AGE</Button>
+        {iconsAndNames.map((iconAndName, idx) => (
+          <Button
+            key={idx}
+            w='60px'
+            h='auto'
+            p='0'
+            mr='1rem'
+            bg='hsla(0, 0%, 100%, .3)'
+            onClick={() => handleLayoutChange(iconAndName.name)}
+          >
+            <Image
+              src={iconAndName.icon}
+              h='100%'
+              objectFit='contain'
+              p='.5em'
+            />
+          </Button>
+        ))}
       </Box>
     </Box>
   )
