520faba0da236e3fb30568984bee64f7dc54dbab
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-07-17 14:41:21 -0400

    Test 100vh mobile fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,22 @@ import { activeNodeIDAtom } from 'utils/atoms.js'
 import Menu from 'components/Menu'
 import Layouts from 'components/Layouts'
 import ProfileCard from 'components/ProfileCard'
+import { useEffect } from 'react'
 // import ProfileCardChumma from 'components/ProfileCardChumma'
 
 export default function App () {
   const activeNodeID = useRecoilValue(activeNodeIDAtom)
+
+  useEffect(() => {
+    // add event listener on resize to handle mobile navbar issue
+    document.querySelector(':root').style.setProperty('--vh', window.innerHeight / 100 + 'px')
+    const resize = window.addEventListener('resize', () => {
+      document.querySelector(':root').style.setProperty('--vh', window.innerHeight / 100 + 'px')
+    })
+    return () => {
+      window.removeEventListener('resize', resize)
+    }
+  }, [])
 
   return (
     <>
