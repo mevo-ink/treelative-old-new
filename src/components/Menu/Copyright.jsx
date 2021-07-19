e253b357826208f8d67f57aaae3cfab016e1a20b
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-07-19 15:31:52 -0400

    Rephrase copyright text

--- src/components/Menu/Copyright.jsx
+++ src/components/Menu/Copyright.jsx
@@ -3,10 +3,10 @@ import { Text, Link } from '@chakra-ui/react'
 export default function Copyright () {
   return (
     <Text textAlign='center' fontSize='.6rem'>
-      &#xA9; 2020-2021. All Rights Reserved by
-      <Link href='https://arun99.dev' isExternal fontWeight='600'> Arun </Link>
-      &#38;
-      <Link href='https://jana19.dev' isExternal fontWeight='600'> Jana</Link>.
+      &#xA9; {new Date().getFullYear()}. Designed by &nbsp;
+      <Link href='https://arun99.dev' isExternal fontWeight='600'>Arun</Link>
+      &nbsp; &#38; &nbsp;
+      <Link href='https://jana19.dev' isExternal fontWeight='600'>Jana</Link>.
     </Text>
   )
 }
