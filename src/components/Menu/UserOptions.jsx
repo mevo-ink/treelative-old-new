0b5fb2bf2d30d48abe22734f8f096a12faf264f5
Arun <friday99@outlook.it>
2021-08-05 17:09:23 +0200

    group user options

---
+++ src/components/Menu/UserOptions.jsx
@@ -0,0 +1,18 @@
+import { useQuery } from 'urql'
+
+import { WHO_AM_I } from 'graphql/queries/auth'
+
+import FindMe from 'components/Menu/UserOptions/FindMe'
+import Profile from 'components/Menu/UserOptions/Profile'
+import CreateUser from 'components/Menu/UserOptions/CreateUser'
+
+export default function UserOptions ({ onClose }) {
+  const [result] = useQuery({ query: WHO_AM_I })
+  return (
+    <>
+      {result.data?.whoAmI.isAdmin && <CreateUser />}
+      {result.data?.whoAmI && <Profile onClose={onClose} authUser={result.data?.whoAmI} />}
+      {result.data?.whoAmI && <FindMe onClose={onClose} user={result.data?.whoAmI} size='sm' />}
+    </>
+  )
+}
