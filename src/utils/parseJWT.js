3fe961aeb0a6290bcb3d65050bd40a69a5e9bf8f
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-08-14 12:36:32 -0400

    Prepare for nextJS migration

--- src/utils/parseJWT.js
+++
@@ -1,6 +0,0 @@
-export default function parseJwt (token) {
-  if (!token) return {}
-  const base64Payload = token.split('.')[1]
-  const payload = Buffer.from(base64Payload, 'base64')
-  return JSON.parse(payload.toString())
-}
