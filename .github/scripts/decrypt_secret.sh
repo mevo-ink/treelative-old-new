f7bbbb56a27c67465aa9b1d7f55c9ca62aa272e0
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-08-14 19:19:31 -0400

    Fix firebaseAuth file location in github script

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -4,4 +4,4 @@
 
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$DECRYPT_PASSPHRASE" --output src/utils/firebaseAuth.json src/utils/firebaseAuth.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$DECRYPT_PASSPHRASE" --output src/sever/utils/firebaseAuth.json src/sever/utils/firebaseAuth.json.gpg
