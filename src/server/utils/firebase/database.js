7a8c23767b0c7fc3a9c2990287b89ebb972c81dc
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-08-17 00:47:03 -0400

    Remove all unused firestore helper methods

--- src/server/utils/firebase/database.js
+++ src/server/utils/firebase/database.js
@@ -37,33 +37,9 @@ db.findOneByIdAndUpdate = async (collection, id, input) => {
   return (await ref.get()).data()
 }
 
-db.findOneAndUpdate = async (collection, filters, input) => {
-  let queryRef = db.collection(collection)
-  for (const [key, filter] of Object.entries(filters)) {
-    console.log(key, '==', filter)
-    queryRef = queryRef.where(key, '==', filter)
-  }
-  const results = await queryRef.get()
-  const user = results.docs[0].data()
-  return db.findOneByIdAndUpdate(collection, user.id, input)
-}
-
 db.deleteOneById = async (collection, id) => {
   const ref = db.collection(collection).doc(id)
   await ref.delete()
-}
-
-db.deleteChild = async (collection, childID, parentID) => {
-  // remove child from parent
-  const parent = (await db.collection(collection).doc(parentID).get()).data()
-  parent.children = parent.children.filter(id => id !== childID)
-  await db.collection(collection).doc(parentID).set({ children: parent.children }, { merge: true })
-  // remove parent from child
-  const child = (await db.collection(collection).doc(childID).get()).data()
-  child.parents = await child.parents.filter(id => id !== parentID)
-  console.log(child.parents)
-  await db.collection(collection).doc(childID).set({ parents: child.parents }, { merge: true })
-  return true
 }
 
 export default db
