e784cd550bd4a8292e44e9824b518c7f899b15af
Arun <friday99@outlook.it>
2021-08-17 10:16:38 +0200

    fix getAgeData mutation

--- src/server/resolvers/queries/layouts/getAgeData.js
+++ src/server/resolvers/queries/layouts/getAgeData.js
@@ -1,20 +1,22 @@
 export default async (parent, args, context, info) => {
-  const users = await context.db.findAll('users', { dateOfBirth: { '!=': null } })
-  const unknownCount = (await context.db.collection('users').where('dateOfBirth', '==', null).get()).docs.length
-
   const data = {}
+  let unknownCount = 0
+  const users = await context.db.findAll('users')
 
+  // get avatar from ui-avatars.com/api using fullName and upload to storage avatar bucket
   for (const user of users) {
-    const birthYear = new Date(user.dateOfBirth).toISOString().slice(0, 4)
+    if (user.dateOfBirth) {
+      const birthYear = new Date(user.dateOfBirth).toISOString().slice(0, 4)
 
-    user.avatar = `${process.env.STORAGE_ENDPOINT}/avatars/${user.id}.png`
-    user.brokenAvatar = `https://ui-avatars.com/api/?name=${user.fullName}&background=random&rounded=true&font-size=0.5&bold=true`
+      user.avatar = `${process.env.STORAGE_ENDPOINT}/avatars/${user.id}.png`
+      user.brokenAvatar = `https://ui-avatars.com/api/?name=${user.fullName}&background=random&rounded=true&font-size=0.5&bold=true`
 
-    if (data[birthYear]) {
-      data[birthYear] = [...data[birthYear], user]
-    } else {
-      data[birthYear] = [user]
-    }
+      if (data[birthYear]) {
+        data[birthYear] = [...data[birthYear], user]
+      } else {
+        data[birthYear] = [user]
+      }
+    } else unknownCount = unknownCount + 1
   }
 
   return {
