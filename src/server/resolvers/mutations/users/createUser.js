e5044dbd5f7888d639d0857ee244e88fce8a68e0
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-08-20 01:48:42 -0400

    Add deafult null values on user creation

--- src/server/resolvers/mutations/users/createUser.js
+++ src/server/resolvers/mutations/users/createUser.js
@@ -7,7 +7,32 @@ import fetch from 'node-fetch'
 export default async (parent, args, context, info) => {
   isAdmin(context)
 
-  const user = await context.db.create('users', args.input)
+  const user = await context.db.create('users', {
+    isAdmin: false,
+    isPublic: false,
+    fullName: null,
+    shortName: null,
+    email: null,
+    isEmailVerified: false,
+    phoneNumber: null,
+    birthLocation: null,
+    currentLocation: null,
+    marriageLocation: null,
+    dateOfBirth: null,
+    dateOfMarriage: null,
+    dateOfDeath: null,
+    social: {
+      facebook: null,
+      twitter: null,
+      instagram: null,
+      linkedin: null,
+      website: null
+    },
+    partner: null,
+    parents: [],
+    children: [],
+    ...args.input
+  })
 
   // download default user avatar from ui-avatars and upload to storage
   const url = `https://ui-avatars.com/api/?name=${user.fullName}&background=random&rounded=true&font-size=0.5&bold=true`
