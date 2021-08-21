2f23a80714d801ba756c5ec3d11cc381b28af43b
Arun <friday99@outlook.it>
2021-08-21 11:38:32 +0200

    add redirect to /auth/login, if not authenticated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
 import { useRecoilValue } from 'recoil'
 import { activeNodeIDAtom } from 'utils/atoms.js'
 
@@ -9,6 +12,15 @@ import useServiceWorker from 'hooks/useServiceWorker'
 
 export default function Home () {
   useServiceWorker()
+
+  const router = useRouter()
+
+  useEffect(() => {
+    if (window.localStorage.getItem('AUTH_SESSION_ID')) {
+      const referrer = window.localStorage.getItem('REDIRECT_REFERRER')
+      router.push(referrer || '/layouts/graph')
+    } else { router.push('/auth/login') }
+  }, [])
 
   const activeNodeID = useRecoilValue(activeNodeIDAtom)
 
