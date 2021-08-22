0f3deb38d90cdcfbdc9c52d9f8006509c431e04a
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-08-22 13:08:38 -0400

    Use absoluteUrl package

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,3 +1,5 @@
+import absoluteUrl from 'next-absolute-url'
+
 import { dedupExchange, fetchExchange } from 'urql'
 import { cacheExchange } from '@urql/exchange-graphcache'
 
@@ -7,17 +9,20 @@ const cache = cacheExchange({
   }
 })
 
-const client = (ssrExchange, ctx) => ({
-  url: `${process.env.NEXT_PUBLIC_DOMAIN}/api/graphql`,
-  exchanges: [dedupExchange, cache, ssrExchange, fetchExchange],
-  fetchOptions: () => {
-    const AUTH_SESSION_ID = ctx
-      ? ctx?.req?.headers?.AUTH_SESSION_ID
-      : window.localStorage.getItem('AUTH_SESSION_ID')
-    return {
-      headers: { AUTH_SESSION_ID }
+const client = (ssrExchange, ctx) => {
+  const { origin } = ctx ? absoluteUrl(ctx.req) : { origin: '' }
+  return {
+    url: `${origin}/api/graphql`,
+    exchanges: [dedupExchange, cache, ssrExchange, fetchExchange],
+    fetchOptions: () => {
+      const AUTH_SESSION_ID = ctx
+        ? ctx?.req?.headers?.AUTH_SESSION_ID
+        : window.localStorage.getItem('AUTH_SESSION_ID')
+      return {
+        headers: { AUTH_SESSION_ID }
+      }
     }
   }
-})
+}
 
 export default client
