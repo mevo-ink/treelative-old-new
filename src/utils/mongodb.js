032ebab5ca7a4c3a670101436d1cbdbbccf529e7
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-09-12 01:47:27 -0400

    Refactor mongoDB connection

--- src/utils/mongodb.js
+++ src/utils/mongodb.js
@@ -10,25 +10,24 @@ const options = {
 let client
 let clientPromise
 
-if (process.env.NODE_ENV === 'development') {
-  // In development mode, use a global variable so that the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(DATABASE_URL, options)
-    global._mongoClientPromise = client.connect()
-  }
-  clientPromise = global._mongoClientPromise
-} else {
-  // In production mode, it's best to not use a global variable.
-  client = new MongoClient(DATABASE_URL, options)
-  clientPromise = client.connect()
-}
-
 // Export a module-scoped MongoClient promise. By doing this in a
 // separate module, the client can be shared across functions.
 export default async function mongoConnect () {
-  const client = await clientPromise
-  const db = client.db()
+  if (process.env.NODE_ENV === 'development') {
+    // In development mode, use a global variable so that the value
+    // is preserved across module reloads caused by HMR (Hot Module Replacement).
+    if (!global._mongoClientPromise) {
+      client = new MongoClient(DATABASE_URL, options)
+      global._mongoClientPromise = await client.connect()
+    }
+    clientPromise = global._mongoClientPromise
+  } else {
+    // In production mode, it's best to not use a global variable.
+    client = new MongoClient(DATABASE_URL, options)
+    clientPromise = client.connect()
+  }
+  const connection = await clientPromise
+  const db = connection.db()
   db.ObjectId = ObjectId
   return db
 }
