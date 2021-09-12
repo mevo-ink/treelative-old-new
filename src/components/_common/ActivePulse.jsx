07b42b2447cff28c3acb7c0ae86b3f66631af871
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-09-11 20:49:10 -0400

    Clear active node pulse after 5 seconds

--- src/components/_common/ActivePulse.jsx
+++ src/components/_common/ActivePulse.jsx
@@ -1,11 +1,27 @@
+import { useEffect } from 'react'
+
 import { Box, keyframes } from '@chakra-ui/react'
 
+import { useSetRecoilState } from 'recoil'
+import { activeNodePulseIDAtom } from 'utils/atoms.js'
+
+const pulse = keyframes`
+  0% { transform: scale(0.1, 0.1); opacity: 0; }
+  50% { opacity: 1;)
+  100% { transform: scale(1.5, 1.5); opacity: 0; }
+`
+
 export default function ActivePulse () {
-  const pulse = keyframes`
-    0% { transform: scale(0.1, 0.1); opacity: 0; }
-    50% { opacity: 1;)
-    100% { transform: scale(1.5, 1.5); opacity: 0; }
-  `
+  const setActiveNodePulseID = useSetRecoilState(activeNodePulseIDAtom)
+
+  useEffect(() => {
+    // clear active node pulse after 5 seconds
+    const timeout = setTimeout(() => {
+      setActiveNodePulseID(null)
+    }
+    , 5000)
+    return () => clearTimeout(timeout)
+  }, [])
 
   return (
     <Box
