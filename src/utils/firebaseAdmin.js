52401fc58ee5a13d1d5e5d96c3ddfd91b9ce0d61
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-10-31 19:43:19 -0400

    Use .env variables for firebase admin credentials

--- src/utils/firebaseAdmin.js
+++ src/utils/firebaseAdmin.js
@@ -2,7 +2,11 @@ import admin from 'firebase-admin'
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.applicationDefault()
+    credential: admin.credential.cert({
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      projectId: process.env.FIREBASE_PROJECT_ID
+    })
   })
 }
 
